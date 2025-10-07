4163a75ef120ddec2e6664170db6a3c09466d952
Muhammadsharif <zabirovms@gmail.com>
2025-10-07 23:52:56 +0500

    feat(locale): Tajik-only; simplify TopBar; new favicon

--- src/services/localizationService.js
+++ src/services/localizationService.js
@@ -1,8 +1,4 @@
-import en from '../locales/ui/en';
-
 export const supportedGameLocales = Object.freeze([
-  {code: 'en', name: 'English'},
-  {code: 'ru', name: 'русский'},
   {code: 'tg', name: 'тоҷикӣ'},
 ]);
 
@@ -15,27 +11,11 @@ export const isLocaleSupported = locale => supportedGameLocalesByCode[locale] !=
 export const getTranslation = locale =>
   import(`../locales/ui/${locale}`)
     .then(translations => ({
-      ...en, // need default keys to english as not all dictionaries are complete
       ...translations
     }));
 
 export const getBestMatchingLocale = () => {
   const defaultLocale = 'tg';
-
-  if (!window.navigator) {
-    return defaultLocale;
-  }
-
-  const browserLocales = navigator.languages !== undefined ? navigator.languages : [navigator.language];
-
-  for (let i = 0, l = browserLocales.length; i < l; i++) {
-    const baseLocale = browserLocales[i].toLocaleLowerCase().split('-')[0];
-
-    if (supportedGameLocalesByCode[baseLocale]) {
-      return baseLocale;
-    }
-  }
-
   return defaultLocale;
 };
 
